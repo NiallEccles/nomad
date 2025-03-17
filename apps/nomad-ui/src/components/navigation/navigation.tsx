7fd14201efd768330faef8c8a5502a14fe073922
EcclesN <Niall.Eccles@hmhco.com>
2025-03-17 11:28:38 +0000

    Create navigation

--- apps/nomad-ui/src/components/navigation/navigation.tsx
+++ apps/nomad-ui/src/components/navigation/navigation.tsx
@@ -4,7 +4,7 @@ import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@her
 export const Navigation = () => (
   <Navbar>
     <NavbarBrand>
-      <p className="font-bold text-inherit">ACME</p>
+      <p className="font-bold text-inherit">Nomad</p>
     </NavbarBrand>
     <NavbarContent className="hidden sm:flex gap-4" justify="center">
       <NavbarItem>
