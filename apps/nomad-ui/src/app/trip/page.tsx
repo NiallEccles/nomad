608c3323ff0b85a23217aed581ea8d0404d8d7db
EcclesN <Niall.Eccles@hmhco.com>
2025-03-17 22:44:04 +0000

    Fix trip planner

--- apps/nomad-ui/src/app/trip/page.tsx
+++ apps/nomad-ui/src/app/trip/page.tsx
@@ -1,11 +1,10 @@
-// import { TripPlanner } from '@/components/trip-planner/trip-planner';
+import { TripPlanner } from '@/components/trip-planner/trip-planner';
 
 
 export default function Index() {
   return (
     <div className='flex flex-row min-h-screen relative p-3'>
-      {/*<TripPlanner/>*/}
-      <h1>hello</h1>
+      <TripPlanner/>
     </div>
   );
 }
