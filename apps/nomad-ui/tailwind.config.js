2a20f6e20e407529fb01f2575009bcddf9658f84
EcclesN <Niall.Eccles@hmhco.com>
2025-03-17 11:26:32 +0000

    Add tailwind

--- apps/nomad-ui/tailwind.config.js
+++ apps/nomad-ui/tailwind.config.js
@@ -1,4 +1,5 @@
 // const { createGlobPatternsForDependencies } = require('@nx/next/tailwind');
+const {heroui} = require("@heroui/react");
 
 // The above utility import will not work if you are using Next.js' --turbo.
 // Instead you will have to manually add the dependent paths to be included.
@@ -14,10 +15,12 @@ module.exports = {
   content: [
     './{src,pages,components,app}/**/*.{ts,tsx,js,jsx,html}',
     '!./{src,pages,components,app}/**/*.{stories,spec}.{ts,tsx,js,jsx,html}',
+    "../../node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
     //     ...createGlobPatternsForDependencies(__dirname)
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [heroui()],
 };
