28d35e4ae560e8433a5c4a64dbf7f73bb63faeb4
EcclesN <Niall.Eccles@hmhco.com>
2025-03-17 22:44:23 +0000

    Fix localstorage error

--- apps/nomad-ui/src/components/trip-planner/trip-planner.tsx
+++ apps/nomad-ui/src/components/trip-planner/trip-planner.tsx
@@ -1,19 +1,26 @@
 'use client';
 
 import { Trip } from '@/types/trip';
-// import {useDateFormatter,parseAbsoluteToLocal} from "@react-aria/i18n";
 import { CalendarDate, getLocalTimeZone } from '@internationalized/date';
+import { Adapter } from '@/adapter';
+import { useEffect, useState } from 'react';
 
 
 export const TripPlanner = () => {
-  const localTrip = localStorage.getItem('trip')
-  const basicDetails: Trip | undefined = localTrip ? JSON.parse(localTrip) : undefined;
-  const startDate = new CalendarDate(basicDetails?.trip_duration?.start.year!, basicDetails?.trip_duration?.start.month!, basicDetails?.trip_duration?.start.day!)
-  const endDate = new CalendarDate(basicDetails?.trip_duration?.end.year!, basicDetails?.trip_duration?.end.month!, basicDetails?.trip_duration?.end.day!)
+  const adapter = new Adapter('local');
+  const [tripData, setTripData] = useState<Trip>();
+
+  useEffect(() => {
+    adapter.get().then(trip => {
+      setTripData(trip)
+    });
+  }, []);
+  const startDate = new CalendarDate(tripData?.trip_duration?.start.year!, tripData?.trip_duration?.start.month!, tripData?.trip_duration?.start.day!)
+  const endDate = new CalendarDate(tripData?.trip_duration?.end.year!, tripData?.trip_duration?.end.month!, tripData?.trip_duration?.end.day!)
   return (
     <div>
-      <h1 className='text-lg font-bold'>{basicDetails?.trip_name}</h1>
-      <h2>{basicDetails?.destination}</h2>
+      <h1 className='text-lg font-bold'>{tripData?.trip_name}</h1>
+      <h2>{tripData?.destination}</h2>
       <h3>{startDate.toDate(getLocalTimeZone()).toLocaleDateString()}</h3>
       <h3>{endDate.toDate(getLocalTimeZone()).toLocaleDateString()}</h3>
     </div>
