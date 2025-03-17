5ecfb4d1772d94677745da494bf737a1428adcd9
EcclesN <Niall.Eccles@hmhco.com>
2025-03-17 23:11:19 +0000

    Add some styles for trip

--- apps/nomad-ui/src/components/trip-planner/trip-planner.tsx
+++ apps/nomad-ui/src/components/trip-planner/trip-planner.tsx
@@ -1,28 +1,45 @@
 'use client';
 
 import { Trip } from '@/types/trip';
+// import { Button, Card, CardBody } from "@heroui/react";
 import { CalendarDate, getLocalTimeZone } from '@internationalized/date';
 import { Adapter } from '@/adapter';
 import { useEffect, useState } from 'react';
 
-
 export const TripPlanner = () => {
   const adapter = new Adapter('local');
   const [tripData, setTripData] = useState<Trip>();
+  // const [days, setDays] = useState<any[]>([1]);
 
   useEffect(() => {
     adapter.get().then(trip => {
       setTripData(trip)
     });
   }, []);
+
+  // const handleClick = () => {
+  //   setDays(prevState => [...prevState, 1]);
+  // }
   const startDate = new CalendarDate(tripData?.trip_duration?.start.year!, tripData?.trip_duration?.start.month!, tripData?.trip_duration?.start.day!)
   const endDate = new CalendarDate(tripData?.trip_duration?.end.year!, tripData?.trip_duration?.end.month!, tripData?.trip_duration?.end.day!)
   return (
-    <div>
-      <h1 className='text-lg font-bold'>{tripData?.trip_name}</h1>
-      <h2>{tripData?.destination}</h2>
-      <h3>{startDate.toDate(getLocalTimeZone()).toLocaleDateString()}</h3>
-      <h3>{endDate.toDate(getLocalTimeZone()).toLocaleDateString()}</h3>
+    <div className="border-5 p-5 rounded-2xl flex flex-col justify-between h-60">
+      <div>
+        <h1 className="text-4xl font-bold">{tripData?.trip_name}</h1>
+        <h2>{tripData?.destination}</h2>
+      </div>
+      <div>
+        <h3>{startDate.toDate(getLocalTimeZone()).toLocaleDateString()}</h3>
+        <h3>{endDate.toDate(getLocalTimeZone()).toLocaleDateString()}</h3>
+      </div>
+      {/*{days?.map((day, i) => (*/}
+      {/*  <Card key={i}>*/}
+      {/*    <CardBody>*/}
+      {/*      <p>Make beautiful websites regardless of your design experience.</p>*/}
+      {/*    </CardBody>*/}
+      {/*  </Card>*/}
+      {/*))}*/}
+      {/*<Button color="primary" onPressEnd={handleClick}>Button</Button>*/}
     </div>
-  )
+  );
 }
