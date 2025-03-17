b2bf9c0a125f2c310206e4f2a848e227300d821e
EcclesN <Niall.Eccles@hmhco.com>
2025-03-17 17:58:27 +0000

    Create trip page

--- apps/nomad-ui/src/app/trip/page.tsx
+++ apps/nomad-ui/src/app/trip/page.tsx
@@ -1,19 +1,10 @@
-import marketingImage from '@/assets/marketing.jpg';
-import {Button, Link} from "@heroui/react";
+import { TripPlanner } from '@/components/trip-planner/trip-planner';
 
 
-export const Trip = () => (
-  // eslint-disable-next-line @next/next/no-img-element
-  <div
-    className="h-full min-h-svh flex-1 bg-cover bg-top hidden md:block"
-    style={{ backgroundImage: 'url(' + marketingImage.src + ')' }}>
-    <div className='absolute right-3 bottom-3'>
-      <Button as={Link} size={'sm'} isExternal showAnchorIcon href="https://unsplash.com/@seefromthesky?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-        Photo by Ishan @seefromthesky
-      </Button>
+export default function Index() {
+  return (
+    <div className='flex flex-row min-h-screen relative p-3'>
+      <TripPlanner/>
     </div>
-  </div>
-);
-
-// Photo by <a href="https://unsplash.com/@seefromthesky?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ishan @seefromthesky</a> on <a href="https://unsplash.com/photos/aerial-view-of-two-blue-boats-LNdpi8Yzi34?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-
+  );
+}
